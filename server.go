7a633bf8783857628040639d958234ef33358055
Eugene Lubarsky <eugene@patsoftware.com.au>
2018-05-30 17:06:23 +1000

    server.go: configuration options

--- server.go
+++ server.go
@@ -2,17 +2,50 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
+	"time"
 
+	"github.com/intervention-engine/fhir/auth"
 	"github.com/intervention-engine/fhir/server"
 )
 
 func main() {
 	reqLog := flag.Bool("reqlog", false, "Enables request logging -- do NOT use in production")
 	flag.Parse()
-	s := server.NewServer("localhost")
+
+	mongoHost, mongoHostDefined := os.LookupEnv("MONGO_PORT_27017_TCP_ADDR")
+	if !mongoHostDefined {
+		mongoHost = "localhost"
+	}
+	mongoPort, mongoPortDefined := os.LookupEnv("MONGO_PORT_27017_TCP_PORT")
+	if !mongoPortDefined {
+		mongoPort = "27017"
+	}
+
+	mongoHostPort := fmt.Sprintf("%s:%s", mongoHost, mongoPort)
+	fmt.Printf("Mongo host: %s\n", mongoHostPort)
+
+	var MyConfig = server.Config{
+		ServerURL:             "http://localhost:3001",
+		IndexConfigPath:       "config/indexes.conf",
+		DatabaseHost:          mongoHostPort,
+		DatabaseName:          "fhir",
+		DatabaseSocketTimeout: 2 * time.Minute,
+		DatabaseOpTimeout:     90 * time.Second,
+		DatabaseKillOpPeriod:  10 * time.Second,
+		Auth:                  auth.None(),
+		EnableCISearches:      true,
+		CountTotalResults:     true,
+		ReadOnly:              false,
+		EnableXML:             true,
+		Debug:                 true,
+	}
+	s := server.NewServer(MyConfig)
 	if *reqLog {
 		s.Engine.Use(server.RequestLoggerHandler)
 	}
+	// s.Engine.Use(server.RequestLoggerHandler)
 
-	s.Run(server.DefaultConfig)
+	s.Run()
 }
